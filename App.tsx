5c40743da3b8013572af0c59229abf7530051f27
Telath <clement.pouillart@gmail.com>
2023-02-08 09:53:52 +0100

    added view for the api on local

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HomeScreen from "./src/screens/HomeScreen";
+import { HomeScreen } from "./src/screens/HomeScreen";
 
 const App = () => {
   return <HomeScreen />;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,26 +1,73 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  StatusBar,
+  View,
+  FlatList,
+} from "react-native";
+import { Avatar, Card } from "react-native-paper";
 
 import { default as data } from "../../api/data.json";
 
-export default function App() {
+type ItemData = {
+  anime: string;
+  character: string;
+  quote: string;
+};
+
+const LeftContent = (props: object) => (
+  <Avatar.Icon {...props} icon="format-quote-close" />
+);
+
+const Item = ({ anime, character, quote }: ItemData) => {
+  return (
+    <Card>
+      <Card.Title title={anime} subtitle={character} left={LeftContent} />
+      <Card.Content>
+        <Text style={styles.quote}>"{quote}"</Text>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.container}>
-        <Text>{JSON.stringify(data)}</Text>
-      </View>
+      <FlatList
+        data={data}
+        renderItem={({ item }) => (
+          <Item
+            anime={item.anime}
+            character={item.character}
+            quote={item.quote}
+          />
+        )}
+      />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
   safeContainer: {
     flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
+  },
+  quote: {
+    fontStyle: "italic",
+  },
+  container: {
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+  },
+  anime: {
+    fontSize: 32,
   },
 });
